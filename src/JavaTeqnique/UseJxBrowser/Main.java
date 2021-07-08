f7cc6291f6b6a07ba3f0855fe4f7dec5acfd78e6
ksone02 <eksovus@gmail.com>
2021-07-08 15:51:51 +0900

    JxBrowser 연동

--- src/JavaTeqnique/UseJxBrowser/Main.java
+++ src/JavaTeqnique/UseJxBrowser/Main.java
@@ -3,33 +3,92 @@ package JavaTeqnique.UseJxBrowser;
 import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import com.teamdev.jxbrowser.browser.Browser;
 import com.teamdev.jxbrowser.engine.Engine;
 import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.navigation.Navigation;
 import com.teamdev.jxbrowser.view.swing.BrowserView;
 
-public final class Main {
-	public static void main(String[] args) {
+
+public final class Main extends JFrame {
+	
+	private Browser browser;
+	private BrowserView view;
+	private Navigation nav;
+	
+	private JTextField textField = new JTextField(30);
+	private JButton button = new JButton("°Ë»ö");
+	private JPanel panel = new JPanel();
+	
+	public class Event implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			setURL(textField.getText());
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+		
+	}
+	
+	public void setURL(String URL) {
+		nav.loadUrl(URL);
+	}
+	
+	public Main() {
 		Engine engine = Engine.newInstance(EngineOptions.newBuilder(HARDWARE_ACCELERATED)
                 .licenseKey("6P830J66YB74320L8YCSI4AWGNTTNH1C9SWLJVBKVN8S4OXV65C8JVL67LTC55VW87LH")
                 .build());
-		Browser browser = engine.newBrowser();
+		browser = engine.newBrowser();
 		System.setProperty("jxbrowser.license.key",
 				"6P830J66YB74320L8YCSI4AWGNTTNH1C9SWLJVBKVN8S4OXV65C8JVL67LTC55VW87LH");
 
 		SwingUtilities.invokeLater(() -> {
-			BrowserView view = BrowserView.newInstance(browser);
+			view = BrowserView.newInstance(browser);
 
-			JFrame frame = new JFrame("Swing BrowserView");
-			frame.add(view, BorderLayout.CENTER);
-			frame.setSize(700, 500);
-			frame.setVisible(true);
+			setLayout(new BorderLayout());
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setTitle("JxBrowser");
+			
+			setSize(1280, 720);
+			setVisible(true);
+			
+			add(panel, BorderLayout.NORTH);
+			add(view, BorderLayout.CENTER);
 
 			browser.navigation().loadUrl("https://www.google.com");
+			
+			panel.add(textField);
+			panel.add(button);
+			button.addMouseListener(new Event());
 		});
+		
+		
+	}
+	public static void main(String[] args) {
+		Main main = new Main();
 	}
 }
